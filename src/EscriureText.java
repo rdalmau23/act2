a4087e1cb409a1ac3d4c94685cd90a406fd93ac1
rdalmau23 <rdalmau23@ilg.cat>
2024-11-20 12:18:24 +0100

    act1 done

---
+++ src/EscriureText.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ @author Rafel Dalmau
+ @Date 6/11/2024
+*/
+
+public class EscriureText {
+    public static void main(String[] args) {
+                Scanner input = new Scanner(System.in);
+                System.out.println("Introdueix la ruta del fitxer: ");
+                String path = input.nextLine();
+                System.out.println("Introdueix text ");
+                String text = input.nextLine();
+
+                text=invertir(text);
+               
+                escriureFitxer(path, text);
+                llegirFitxer(path);
+    
+    }
+
+    public static void escriureFitxer (String path, String text){
+        try (PrintStream writer = new PrintStream(new File(path))) {
+            writer.println(text);
+            writer.close();
+        }
+        catch (Exception e) {
+        System.out.println("Error" + e.getMessage());
+        }
+    }
+
+    public static void llegirFitxer (String path) {
+        File file = new File(path);
+        try (Scanner scanner = new Scanner(file)) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            
+        }
+        catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+            }
+    }
+
+    public static String invertir(String text) {
+        StringBuilder invertido = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                invertido.append(Character.toLowerCase(c));
+            } else {
+                invertido.append(Character.toUpperCase(c));
+            }
+        }
+        return invertido.toString();
+    }
+}
+    
